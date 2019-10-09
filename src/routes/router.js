9592f48190ed26684ca433eed8825a15102ceb34
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-10-09 01:28:53 -0300

    Delete Quiz

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,5 +5,6 @@ const QuizesController = require('../controllers/QuizesController')
 router.get('/quizes', QuizesController.getAll)
 router.get('/quizes/:id', QuizesController.getById)
 router.post('/quizes', QuizesController.save)
+router.delete('/quizes/:id', QuizesController.delete)
 
 module.exports = router
