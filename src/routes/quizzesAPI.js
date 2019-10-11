69abc9734d272f6f9ff361b73e62a22c65ca090d
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-10-10 23:37:42 -0300

    Authentication middleware

--- src/routes/quizzesAPI.js
+++ src/routes/quizzesAPI.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middlewares/auth')
 const QuizzesController = require('../controllers/QuizzesController')
 
+router.use(authMiddleware)
 router.get('/', QuizzesController.getAll)
 router.get('/:id', QuizzesController.getById)
 router.post('/', QuizzesController.save)
