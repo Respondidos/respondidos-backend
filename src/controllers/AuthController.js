b9c14ed92e84f6a1199b7f2cf0730651f1872c1e
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-10-10 23:11:53 -0300

    User authentication with JWT

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,12 +1,46 @@
 const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth.json')
+
+function generateToken(params = {}) {
+  const token = jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400
+  })
+  return token
+}
 
 module.exports = {
-  async save (req, res) {
+  async register (req, res) {
     try {
       const user = await User.create(req.body)
+
+      user.password = undefined
+
       return res.send({ user })
     } catch(ex) {
-      return res.status(400).json({ error: 'Registration failed' });
+      return res.status(400).json({ error: 'Falha no registro' });
+    }
+  },
+  async authenticate (req, res) {
+    const { email, password } = req.body
+    try {
+      const user = await User.findOne({ email }).select('+password')
+      if (!user) {
+        return res.status(400).json({ error: 'Usuário não encontrado' })
+      }
+      if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).json({ error: 'Senha inválida' })
+      }
+
+      user.password = undefined
+
+      return res.json({
+        user,
+        token: generateToken({id: user.id})
+      });
+    } catch (ex) {
+      return res.status(400).json({ error: 'Houve um erro ao retornar usuário' })
     }
   }
 }
--- src/routes/authAPI.js
+++ src/routes/authAPI.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 
-router.post('/register', AuthController.save)
+router.post('/register', AuthController.register)
+router.post('/authenticate', AuthController.authenticate)
 
 module.exports = router
