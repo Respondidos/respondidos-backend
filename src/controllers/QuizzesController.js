8e56a8bdcfae4aac49e800926faef8b115140144
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-10-31 14:54:55 -0300

    Questions CRUD

--- src/controllers/QuizzesController.js
+++ src/controllers/QuizzesController.js
@@ -4,34 +4,34 @@ module.exports = {
   async getAll (req, res) {
     try {
       const quizes = await Quiz.find().populate('info.creator')
-      res.json(quizes)
+      return res.json(quizes)
     } catch (ex) {
-      res.json(ex)
+      return res.json(ex)
     }
   },
   async getById (req, res) {
     try {
       const quiz = await Quiz.find({ _id: req.params.id }).populate('info.creator')
-      res.json(quiz)
+      return res.json(quiz)
     } catch (ex) {
-      res.json(ex)
+      return res.json(ex)
     }
   },
   async save (req, res) {
     try {
       const myQuiz = req.body
       const quiz = await Quiz.create(myQuiz).populate('info.creator')
-      res.json(quiz)
+      return res.json(quiz)
     } catch (ex) {
-      res.json(ex)
+      return res.json(ex)
     }
   },
   async delete (req, res) {
     try {
       const quiz = await Quiz.deleteOne({ _id: req.params.id })
-      res.json(quiz)
+      return res.json(quiz)
     } catch (ex) {
-       res.json(ex)
+      return res.json(ex)
     }
   }
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const quizzesAPI = require('./quizzesAPI')
+const questionsAPI = require('./questionsAPI')
 const authAPI = require('./authAPI')
 
 router.use('/quizzes', quizzesAPI)
+router.use('/questions', questionsAPI)
 router.use('/auth', authAPI)
 
 module.exports = router
